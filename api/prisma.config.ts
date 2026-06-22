7396783f2029cba12dfabc45d9ad8e87eb7237be
João <joao_todescato@unochapeco.edu.br>
2026-06-22 18:29:21 -0300

    Update prisma.config.ts

--- api/prisma.config.ts
+++ api/prisma.config.ts
@@ -5,5 +5,9 @@ export default defineConfig({
   migrations: {
     path: "prisma/migrations",
   },
-  datasourceUrl: process.env.DATABASE_URL,
+  datasourceUrl: () => {
+    const url = process.env.DATABASE_URL;
+    if (!url) throw new Error("DATABASE_URL não definida");
+    return url;
+  },
 });
